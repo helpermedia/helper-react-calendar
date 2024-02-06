36a230ac84fd08a409c16b5d425ab886bf1f31bc
Marcel Ritsema <info@helpermedia.com>
2024-02-06 21:47:31 +0100

    Sass added

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import sass from 'sass'
 import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
@@ -8,4 +9,11 @@ export default defineConfig({
     react(),
     svgr()
   ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
 })
