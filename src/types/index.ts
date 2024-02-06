502792f8eaf0073d95172afe925cbddc81047465
Marcel Ritsema <info@helpermedia.com>
2024-02-06 22:25:12 +0100

    Calendar types added

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+export type CalMonth = {
+  year: number
+  month: number
+  weeks: CalWeek[]
+  selectableDates?: Date[]
+  activeDates?: Date[]
+}
+
+export type CalWeek = {
+  weekNumber: number
+  days: CalDay[]
+}
+
+export type CalDay = {
+  date: Date
+  dayOfTheWeek: number
+  classNames?: string[]
+}
+
+export type Highlight = {
+  dates?: Date[]
+  dateRange?: Date[]
+  names: string[]
+}
+
+export type DayClass = {
+  day: number
+  names: string[]
+}
