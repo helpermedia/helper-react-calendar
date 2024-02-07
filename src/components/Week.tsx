b9a8c200e8352824084f24ba421e018900780e46
Marcel Ritsema <info@helpermedia.com>
2024-02-07 21:55:22 +0100

    Add empty days to begin and end of month

--- src/components/Week.tsx
+++ src/components/Week.tsx
@@ -12,13 +12,33 @@ export default function Month({
   locale
 }: Props): React.ReactElement {
 
+  // Add empty days to the beginning of end the week
+  const emptyDays = (position: string) => {
+    const emptyDays = []
+    let numberOfDays = 0
+    if (position === 'begin') {
+      numberOfDays = week.days[0].dayOfTheWeek - 1
+    }
+    else {
+      numberOfDays = 7 - week.days[week.days.length - 1].dayOfTheWeek
+    }
+    for (let i = 0; i < numberOfDays; i++) {
+      emptyDays.push(<div key={i + 1} className="cal-day empty"></div>)
+    }
+    return emptyDays
+  }
+
   return (
     <div className={`cal-week week-${week.weekNumber}`}>
+      {emptyDays('begin')}
+
       {week.days.map(day => (
-        <div key={day.date.toString()} className="cal-day">
+        <div key={day.dayOfTheWeek} className="cal-day">
           {format(day.date, 'd', { locale: locale })}
         </div>
       ))}
+
+      {emptyDays('end')}
     </div>
   )
 }
